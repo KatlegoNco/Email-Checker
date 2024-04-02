1d150168e0a25990810f2eda58310b244e81a195
Grealish10k <katlegoncongwane20@gmail.com>
2024-04-02 13:35:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Text.RegularExpressions;
+
+namespace EmailCheckerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an email address: ");
+            string email = Console.ReadLine();
+
+            if (IsValidEmail(email))
+            {
+                //if valid
+                Console.WriteLine($"The email address entered '{email}' is valid.");
+            }
+            else
+            {   
+                //if not valid
+                Console.WriteLine($"The email address entered '{email}' is not valid.");
+            }
+
+            Console.ReadLine();
+        }
+
+         static bool IsValidEmail(string email)
+        {
+            // Regular expression pattern for email validation
+            string pattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+
+            // Check if the email matches the pattern
+            return Regex.IsMatch(email, pattern, RegexOptions.IgnoreCase);
+        }
+    }
+}
